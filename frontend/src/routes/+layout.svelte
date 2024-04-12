cbaf4ca178dcb1681e669cc3fbb04e3cee1e1ac5
Casey Primozic <me@ameo.link>
2024-04-11 21:29:08 -0700

    Commit protos

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  export const prerender = true;
-
   import '../app.css';
   import { initGlobalToaster } from '../components/Common/GlobalToaster';
   import GlobalToaster from '../components/Common/GlobalToaster.svelte';
